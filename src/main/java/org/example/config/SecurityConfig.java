5eb665b9013fb0b79575eea50fbdbcbd716d8019
asanmiguel12 <adrianchristiansanmiguel@gmail.com>
2026-01-29 11:46:15 -0800

    dd an in-memory UserDetailsService bean

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -13,6 +13,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 
 @Configuration
 public class SecurityConfig {
@@ -62,6 +69,23 @@ public class SecurityConfig {
         ) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+    UserDetails user = User.builder()
+            .username("test")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER")
+            .build();
+
+    return new InMemoryUserDetailsManager(user);
+}
+
+    @Bean
+        public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
 
 }
 
